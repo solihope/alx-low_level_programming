65b7ca9a1bf421794ba9fae28542c541efe4596b
soli <hosgreet15@gmail.com>
2022-12-08 14:11:04 -0800

    print return 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,4 @@
  *Return: 0
  */
 int main(void)
-{    
-	puts("\"Programming is like building a multilingual puzzle");
-	    return (0);
-}
-
+{puts("\"Programming is like building a multilingual puzzle"); return (0);}
