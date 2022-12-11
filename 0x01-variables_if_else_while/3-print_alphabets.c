43d35606f6818210ba4b1c976b9575361b0541ed
soli <hosgreet15@gmail.com>
2022-12-11 14:41:13 -0800

    if else index  3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,9 +10,11 @@ int main(void)
 {
 	char low;
 
-	for(low='a';low<='z';low++)
+	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
-	for(low='A';low<='Z';low++)
-		putchar(low);putchar('\n');
+	for (low = 'A'; low <= 'Z'; low++)
+		putchar(low);
+	putchar('\n');
+
 	return (0);
 }
