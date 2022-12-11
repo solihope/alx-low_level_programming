044a6a3bd0bad94c98212e4820a3b82b259b8eed
soli <hosgreet15@gmail.com>
2022-12-11 14:09:47 -0800

    if else 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
@@ -24,5 +22,6 @@ int main(void)
 		printf("%s %d is %d and is 0\n", str, n, L);
 	else if (L < 6)
 		printf("%s %d is %d and is less than 6 and not 0\n", str, n, L);
+	
 	return (0);
 }
