667b7ac220e7a21550f54d07bba88466adb00594
soli <hosgreet15@gmail.com>
2022-12-11 15:15:44 -0800

    again

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-
 /**
- *main-Entrypoint
+ * main - Entry point
  *
- *Return:Always0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char low;
-	
+
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
 	printf("\n");
