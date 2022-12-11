ff0d5dd1ebc40cf84bf4c9f5c7b4c163f7c5af4b
soli <hosgreet15@gmail.com>
2022-12-11 14:25:18 -0800

    if else index  2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,13 +8,12 @@
  *Return:Always0 (Success)
  */
 int main(void)
-
 {
 	char low;
 	
-	for (low= 'a'; low <= 'z'; low++)
+	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
 	printf("\n");
 
-	return(0);
+	return (0);
 }
