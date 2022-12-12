6c671f9cc6699d49739da6564c4c66a89bbb6656
soli <hosgreet15@gmail.com>
2022-12-12 12:38:46 -0800

    101 adv

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int s = 0, e = 7, s2 = 1, ez =8, s3 = 2, e3 = 9;
+
+	while (s <= e)
+	{
+		s2 = s + 1;
+		while (s2 <= e2)
+		{
+			s3 = s2 + 1;
+			while (s3 <= e3)
+			{
+				putchar(s + '0');
+				putchar(s2 + '0');
+				putchar(s3 + '0');
+
+				if (s != e)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				s3++;
+			}
+			s2++;
+		}
+		s++;
+	}
+	putchar('\n');
+
+	return (0);
+}
