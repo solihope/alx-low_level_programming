0ff037bb8ce8b7b55fb194b865ef810107c5a904
soli <hosgreet15@gmail.com>
2022-12-15 11:25:46 -0800

    1 main

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("infinite look incoming :(\n)");
+
+	i = 0;
+	/*
+	 * while (i < 10)
+	 * {
+	 *        putchar(i);
+	 * }
+	 */
+	printf("infinite loop avoided! \\o/\n");
+
+	return (0);
+}
