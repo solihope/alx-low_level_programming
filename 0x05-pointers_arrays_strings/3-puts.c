e1dd4f423991743b502c5a0a298092157b4f9f77
soli <hosgreet15@gmail.com>
2022-12-20 14:46:51 -0800

    4th string leng

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a line
+ * @str: pointer to the string to print
+ * Return: void
+ */
+void _puts(char *str)
+{
+
+	int i = 0;
+
+	while (str[0])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
