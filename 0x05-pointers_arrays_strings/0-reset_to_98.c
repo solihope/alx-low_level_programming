c39870a8388d4cbb2897cd35d067540c9594e522
soli <hosgreet15@gmail.com>
2022-12-20 14:16:00 -0800

    triel

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,8 @@
  * reset_to_98n - take a pointer to an int parameter and updates
  * the value it pointes to 98
  * @n: point to an int
- * Return: Nothing
+ * 
+ * Return: 0 Nothing
  */
 void reset_to_98(int *n)
 {
