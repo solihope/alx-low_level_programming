ba5f49ee7a114901321eba364386110a473de40b
soli <hosgreet15@gmail.com>
2022-12-20 16:37:36 -0800

    correcting errors

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,13 +13,14 @@
 char *_strcpy(char *dest, char *src)
 {
 	int count = 0;
-	
+
 	while (count >= 0)
 	{
 		*(dest + count) = *(src + count);
+
 		if (*(src + count) == '\0')
 			break;
 		count++;
 	}
-	return (dest);
+	return(dest);
 }
