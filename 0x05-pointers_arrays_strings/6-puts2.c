04e1488128105ecc2dc0a98c15ce91d85ef3476a
soli <hosgreet15@gmail.com>
2022-12-20 16:17:30 -0800

    correcting errors

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,7 +9,7 @@
 void puts2(char *str)
 {
 	int count = 0;
-	
+
 	while (count >= 0)
 	{
 		if (str[count] == '\0')
@@ -17,8 +17,9 @@ void puts2(char *str)
 			_putchar('\n');
 			break;
 		}
-	}
-	if (count % 2 == 0)
-		_putchar(str[count]);
+		if (count % 2 == 0)
+		
+_putchar(str[count]);
 	count++;
+	}
 }
