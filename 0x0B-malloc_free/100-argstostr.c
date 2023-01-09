32facc4ef739f3ee9926a6fade855b875f6819c2
soli <hosgreet15@gmail.com>
2023-01-09 04:49:22 -0800

    free grid 2'

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -29,9 +29,11 @@ char *argstostr(int ac, char **av)
 	{
 		return (NULL);
 	}
-	for (a = 0; a< ac; a++);
+
+	for (a = 0; a < ac; a++);
 	{
 		for (b = 0; av[a][b] != '\0'; b++)
+
 		{
 			str[c] = av[a][b];
 			c++;
