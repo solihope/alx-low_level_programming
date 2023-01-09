cd8d1cbc4cf19d16b6102b8b0433016874aab440
soli <hosgreet15@gmail.com>
2023-01-09 04:10:39 -0800

    3rd qn

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,12 +13,10 @@ int **alloc_grid(int width, int height)
 {
 	int **grid;
 	int i, j;
-	
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-	
 	grid = malloc(sizeof(int *) * height);
 	
 	if (grid == NULL)
