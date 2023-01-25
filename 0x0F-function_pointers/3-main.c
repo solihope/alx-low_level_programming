4269e282bdb79cdee5ff3e07c785a6ee2aed82e6
soli <hosgreet15@gmail.com>
2023-01-25 00:20:22 -0800

    simple operations new

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,7 +1,4 @@
-#include "3-calc.h"
-#include "function_pointers.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include "function_pointers.h"								#include "3-calc.h"
 /**
  * main - performs simple operation
  * @argc: number of arg
@@ -11,9 +8,7 @@
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1, num2;
-	char *op;
-	
-	if (argc != 4)
+	char *op;																			if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
@@ -22,20 +17,16 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	num1 = atoi(argv[1]);
 	op = argv[2];
 	num2 = atoi(argv[3]);
-	
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	
-	if ((*op == '/' && num2 == 0)|| (*op == '%' && num2 == 0))
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	
 	printf("%d\n", get_op_func(op)(num1, num2));
-	
 	return (0);
 }
