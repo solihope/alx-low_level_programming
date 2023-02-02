21d37e3d5f5d461b558d52a233d902cb61498a67
soli <hosgreet15@gmail.com>
2023-02-01 18:10:10 -0800

    3rd but 4th

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -26,9 +26,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		*head = new;
 		return (new);
 	}
+	
 	temp = *head;
 	while (temp->next != NULL)
 		temp = temp->next;
-		temp->next = new;
+		
+	temp->next = new;
 	return (new);
 }
