c6063f47a6f85551840bf32d5019003132647f71
soli <hosgreet15@gmail.com>
2023-02-01 18:26:56 -0800

    4-head free

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,12 +8,12 @@
  */
 void free_listint(listint_t *head)
 {
-	if (head == NULL)
-		return;
+	listint_t *killnode;
 
-	while (head)
+	while (head != NULL)
 	{
-		free(head);
+		killnode = head;
 		head = head->next;
+		free(killnode);
 	}
 }
